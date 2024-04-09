6b3576e92e6ac53c1159c0010641a7e80a5bebe7
gajanand <gajanandkkumawat95@gmail.com>
2024-04-09 16:01:02 +0530

    added the splash screen and connected it with flow.Future.delayed(duration by 5 seconds)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery/modules/pages/view/onboard.dart';
+import 'package:food_delivery/modules/splash/view/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: OnBoard(),
+      home: Splash(),
     );
   }
 }
--- lib/modules/splash/view/splash.dart
+++ lib/modules/splash/view/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/modules/pages/view/onboard.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -9,7 +10,31 @@ class Splash extends StatefulWidget {
 
 class _SplashState extends State<Splash> {
   @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 5), () {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) {
+        return const OnBoard();
+      }), (p) => false);
+    });
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/rajeev_kitchen.png",
+              fit: BoxFit.cover,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

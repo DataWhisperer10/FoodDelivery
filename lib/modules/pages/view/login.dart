d52087b89ddbea9e8e5eea39601ec12d065284c1
gajanand <gajanandkkumawat95@gmail.com>
2024-04-09 10:31:25 +0530

    added the app logo, email and password textfield.

--- lib/modules/pages/view/login.dart
+++ lib/modules/pages/view/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/modules/widget/widget_support.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -30,11 +31,86 @@ class _LoginState extends State<Login> {
                   EdgeInsets.only(top: MediaQuery.of(context).size.height / 3),
               height: MediaQuery.of(context).size.height / 2,
               width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(30),
                       topRight: Radius.circular(30))),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 50, left: 20.0, right: 20.0),
+              child: Column(
+                children: [
+                  Container(
+                    decoration:
+                        BoxDecoration(borderRadius: BorderRadius.circular(40)),
+                    child: Center(
+                        child: Image.asset(
+                      "assets/rajeev_kitchen.png", //app logo image is here
+                      width: MediaQuery.of(context).size.width / 2,
+                      fit: BoxFit.cover,
+                    )),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 2,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Log in",
+                            style: AppWidget.semiBoldTextFieldStyle(),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.email_rounded),
+                                  hintText: "gajanand@example.com",
+                                  hintStyle: TextStyle(
+                                      color: Colors.black54,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400)),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  prefixIcon:
+                                      Icon(Icons.remove_red_eye_rounded),
+                                  hintText: "Password here",
+                                  hintStyle: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.black54)),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10)),
+                            child: Text(
+                              "Forgot Password ?",
+                              style: AppWidget.semiBoldTextFieldStyle(),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
